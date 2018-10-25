28f23130be3dd9ceadb30dc91099048fa4894f70
Simão Menezes <simaomenezesneto@gmail.com>
2018-10-24 22:06:22 -0300

    instalando react, configurando seu preset, teste

--- ex/index.js
+++ ex/index.js
@@ -7,7 +7,7 @@
 //console.log(pessoa.toString());
 
 
-const produto = {
+/*const produto = {
     nome: 'Caneta Bic Preta',
     preco: 1.90,
     desconto: 0.05
@@ -21,3 +21,50 @@ const novoProduto = clone(produto)
 novoProduto.nome = 'Caneta Bic Azul'
 
 console.log(produto, novoProduto);
+*/
+import 'react'
+
+export default props => (
+    <h1>Olá</h1>
+)
+
+console.log("funcionou")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
