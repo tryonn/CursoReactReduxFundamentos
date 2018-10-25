ed937912f4e821b4cb44026953fe53cc779a7547
Simão Menezes <simaomenezesneto@gmail.com>
2018-10-24 21:47:19 -0300

    trabalhando com es2015 e spread, configurando o babel

--- ex/index.js
+++ ex/index.js
@@ -1,2 +1,23 @@
-const logger = require('./logger')
-logger.info('Usando o padrão CommonJS!!!!');
+//const logger = require('./logger')
+//logger.info('Usando o padrão CommonJS!!!!');
+
+//import Pessoa from './pessoa'
+
+//const pessoa = new Pessoa('Show De Bola');
+//console.log(pessoa.toString());
+
+
+const produto = {
+    nome: 'Caneta Bic Preta',
+    preco: 1.90,
+    desconto: 0.05
+}
+
+function clone(objeto) {
+    return { ...objeto }
+}
+
+const novoProduto = clone(produto)
+novoProduto.nome = 'Caneta Bic Azul'
+
+console.log(produto, novoProduto);
