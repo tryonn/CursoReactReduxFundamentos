751ebd07ec02bae6fc84fbe321845c3be638b255
Simão Menezes <simaomenezesneto@gmail.com>
2018-10-25 22:36:20 -0300

    trabalhando com arrow-functions

--- exercicios-react/ex/component.jsx
+++ exercicios-react/ex/component.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 
-export default function() {
-    return <h1>Primeiro Componente</h1>
-}
+/*
+    export default function() {
+        return <h1>Primeiro Componente</h1>
+    }
+*/
+//Arrow Function
+export default () => (
+    <h1>Primeiro Componente</h1>
+)
