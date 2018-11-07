02c34f5d26cc07a2a009a85ae27b732f17240681
Simão Menezes <simaomenezesneto@gmail.com>
2018-11-06 21:25:11 -0300

    evento adicionar

--- todo-app/frontend/src/todo/todo.jsx
+++ todo-app/frontend/src/todo/todo.jsx
@@ -4,11 +4,23 @@ import TodoForm from '../todo/todoForm'
 import TodoList from '../todo/todoList'
 
 export default class Todo extends Component {
+
+    constructor(props){
+        super(props);
+        this.handleAdd = this.handleAdd.bind(this);
+    }
+
+
+    //metodo usado pelo onClick do componente TodoForm
+    handleAdd(){
+        console.log(this);
+    }
+
     render() {
         return (
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
-                <TodoForm />
+                <TodoForm handleAdd={this.handleAdd}/>
                 <TodoList />
             </div>
         )
--- todo-app/frontend/src/todo/todoForm.jsx
+++ todo-app/frontend/src/todo/todoForm.jsx
@@ -8,7 +8,7 @@ export default props => (
             <input id='description' className='form-control' placeholder='Adicione uma tarefa'></input>
         </Grid>
         <Grid cols='12 3 2'>
-            <IconButton style='primary' icon='plus'></IconButton>
+            <IconButton style='primary' icon='plus' onClick={props.handleAdd}></IconButton>
         </Grid>
     </div>
 )
