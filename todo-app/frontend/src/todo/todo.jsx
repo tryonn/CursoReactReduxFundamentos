b47c2cc2acafe2d157936bdb857ec4269e419537
Simão Menezes <simaomenezesneto@gmail.com>
2018-11-07 21:12:03 -0300

    Listando dados e exluindo

--- todo-app/frontend/src/todo/todo.jsx
+++ todo-app/frontend/src/todo/todo.jsx
@@ -1,28 +1,65 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+
+
 import PageHeader from '../template/pageHeader'
 import TodoForm from '../todo/todoForm'
 import TodoList from '../todo/todoList'
+
+const URL = 'http://localhost:3003/api/todos'
 
 export default class Todo extends Component {
 
     constructor(props){
         super(props);
+        this.state = { description: '', list: [] }
+
+        this.handleChange = this.handleChange.bind(this);
         this.handleAdd = this.handleAdd.bind(this);
+        this.handleRemove = this.handleRemove.bind(this);
+
+        this.refresh();
     }
 
+    // metodo para pega dados para a list
+    refresh(){
+        axios.get(`${URL}?sort=creat`)
+            .then(resp => this.setState({...this.state, description: '', list: resp.data}));
+    }
+
+    //metodo para receber a mudança do componente
+    handleChange(e) {
+        this.setState({...this.state, description: e.target.value })
+    }
 
     //metodo usado pelo onClick do componente TodoForm
     handleAdd(){
-        console.log(this);
+        const description = this.state.description
+        axios.post(URL, { description }).then(resp => this.refresh())
+            .catch(function (error) {
+                console.log(error);
+          });
+    }
+
+    // metodo para remover item
+    handleRemove(todo) {
+        axios.delete(`${URL}/${todo._id}`)
+            .then(resp => this.refresh())
     }
 
     render() {
         return (
             <div>
-                <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
-                <TodoForm handleAdd={this.handleAdd}/>
-                <TodoList />
+                <PageHeader 
+                    name='Tarefas' 
+                    small='Cadastro'></PageHeader>
+                <TodoForm 
+                    description={this.state.description} 
+                    handleChange={this.handleChange}
+                    handleAdd={this.handleAdd}/>
+                <TodoList list={this.state.list} handleRemove={this.handleRemove}/>
             </div>
         )
     }
+    
 }
--- todo-app/frontend/src/todo/todoList.jsx
+++ todo-app/frontend/src/todo/todoList.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import IconButton from  '../template/iconButton'
 
-export default propos => (
+export default propos => {
 
-    <div>
-        <h1>
-            Todo List
-        </h1>
-    </div>
-)
+    const renderRows = () => {
+        const list = propos.list || []
+        return list.map(todo => (
+            <tr key={todo._id}>
+                <td>{todo.description}</td>
+                <td>
+                    <IconButton style='danger' icon='trash-o' onClick={ () => propos.handleRemove(todo)}></IconButton>
+                </td>
+            </tr>
+        ))
+    }
+    return (
+        <table className='table'>
+            <thead>
+                <tr>
+                    <th>Descrição</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                {renderRows()}
+            </tbody>
+
+        </table>
+    )
+}
